d2d76c2433d7657efe6a0e576cbdc207dc49870a
SoJoaomesmo <4540746781@estudante.sed.sc.gov.br>
2025-08-27 17:14:47 -0300

    Dark and light themes draft

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,40 @@ class BratzApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bratz',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7DFF)),
-        fontFamily: 'Roboto',
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
       home: const DashboardPage(),
     );
   }
 }
+
+ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7DFF)),
+  fontFamily: 'Roboto',
+  textTheme: TextTheme(
+    bodySmall: TextStyle(color: Colors.white),
+    bodyMedium: TextStyle(color: Colors.white),
+    bodyLarge: TextStyle(color: Colors.white),
+    labelSmall: TextStyle(color: Colors.white),
+    labelMedium: TextStyle(color: Colors.white),
+    labelLarge: TextStyle(color: Colors.white),
+  ),
+  hintColor: Colors.white
+);
+
+ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7DFF)),
+  fontFamily: 'Roboto',
+  textTheme: TextTheme(
+    bodySmall: TextStyle(color: Colors.black),
+    bodyMedium: TextStyle(color: Colors.black),
+    bodyLarge: TextStyle(color: Colors.black),
+    labelSmall: TextStyle(color: Colors.black),
+    labelMedium: TextStyle(color: Colors.black),
+    labelLarge: TextStyle(color: Colors.black),
+  ),
+  hintColor: Colors.black
+);
